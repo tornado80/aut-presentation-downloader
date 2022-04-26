ddc044c6dc22a63c71c0a8516ce012524407bbba
Amirhosein Rajabi <a.h.rajabi@outlook.com>
2022-04-26 14:38:04 +0430

    Add requirements.txt and download.py

--- download.py
+++ download.py
@@ -14,15 +14,15 @@ parser = argparse.ArgumentParser(prog=PROGRAM, description=DESCRIPTION)
 
 parser.add_argument("-u", "--url", required=True, nargs=1, help="presentation url")
 parser.add_argument("-o", "--output", required=True, nargs=1, help="path to output")
-parser.add_argument("-s", "--start", default=(1,), type=int, nargs=1, help="start downloading slides from")
-parser.add_argument("-e", "--end", default=(200,), type=int, nargs=1, help="finish downloading slides when")
+parser.add_argument("-s", "--start", default=1, type=int, nargs=1, help="start downloading slides from")
+parser.add_argument("-e", "--end", default=200, type=int, nargs=1, help="finish downloading slides when")
 
 args = parser.parse_args()
 
 url, = args.url
 output, = args.output
-start, = args.start
-end, = args.end
+start = args.start
+end = args.end
 
 slide_no = start
 
@@ -35,13 +35,15 @@ slides = []
 # TODO: Use "\x1b[1K\r" as the end argument for print
 while slide_no <= end:
     print(f"Downloading slide {slide_no}", end=" ", flush=True)
-    response = requests.get(url + f"/svg/{slide_no}")
+    response = requests.get(url + f"/slide-{slide_no}.png")
     print("DONE")
     if response.status_code == 200:
         path = f"{temp}/{slide_no}.png"
-        print("Converting to PNG", end=" ", flush=True)
-        cairosvg.svg2png(bytestring=response.content, write_to=path)
-        print("DONE")
+        #print("Converting to PNG", end=" ", flush=True)
+        #cairosvg.svg2png(bytestring=response.content, write_to=path)
+        #print("DONE")
+        with open(path, "wb") as f:
+            f.write(response.content)
         slide_no += 1
         slides.append(path)
     else:
